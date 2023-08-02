f4325a4765b74f708c1ff0f8758b48dc853a255b
rauldangio <jojofagqwrt@gmail.com>
2023-08-02 08:55:28 -0300

    criando e importando componente Cardapio

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Cardapio from './pages/Cardapio';
 
 
 const root = ReactDOM.createRoot(
@@ -8,6 +9,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <div>Restaurante do Mestre Chefe Raul</div>
+    <Cardapio />
   </React.StrictMode>
 );
---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,5 @@
+export default function Cardapio(){
+    return (
+        <div>Cardapio Do Restaurante Mojo Dojo Casa House do Grande Mestre Cuca Senhor Doutor Especializado Lindo Chef Raul</div>
+    );
+}
