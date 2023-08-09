2dbda3103f99053fc437ea8cc04f0d6465880d90
rauldangio <jojofagqwrt@gmail.com>
2023-08-09 08:52:40 -0300

    adicionando logo

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,5 +1,13 @@
+import styles from './Cardapio.module.scss'
+import Logo from '../../assets/logo.svg'
+
 export default function Cardapio(){
     return (
-        <div>Cardapio Do Restaurante Mojo Dojo Casa House do Grande Mestre Cuca Senhor Doutor Especializado Lindo Chef Raul</div>
+        <main>
+            <nav className={ styles.menu }>
+            <img src={Logo} alt="Logo do Restaurante"/>
+            {/*Cardapio Do Restaurante Mojo Dojo Casa House do Grande Mestre Cuca Senhor Artista Profissional Admiravel Divino Luxuoso Doutor Magíster Especializado Lindo Danado Explêndido Sensacional Charmoso Chef 5 Estrelas Respeitado Raul*/}
+            </nav>
+        </main>
     );
 }
