c1eb24682a96e49241467c1ecb0bf7ba320f4eca
rauldangio <jojofagqwrt@gmail.com>
2023-08-23 08:58:50 -0300

    criar buscador

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,5 +1,5 @@
 import styles from './Cardapio.module.scss'
-import Logo from '../../assets/logo.svg'
+import Logo from 'assets/logo.svg'
 
 export default function Cardapio(){
     return (
@@ -8,6 +8,14 @@ export default function Cardapio(){
             <img src={Logo} alt="Logo do Restaurante"/>
             {/*Cardapio Do Restaurante Mojo Dojo Casa House do Grande Mestre Cuca Senhor Artista Profissional Admiravel Divino Luxuoso Doutor Magíster Especializado Lindo Danado Explêndido Sensacional Charmoso Chef 5 Estrelas Respeitado Raul*/}
             </nav>
+            <header className={ styles.header }>
+                <div className= { styles.header__text }>
+                  "A casa da massa gostosa e saborosa e deliciosa!"
+                </div>
+            </header>
+            <section>
+              <h3>cardapio</h3>
+            </section>
         </main>
     );
 }
