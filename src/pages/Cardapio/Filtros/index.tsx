3126a2b9e9f0ce4ebe04b222afafff9386809232
rauldangio <jojofagqwrt@gmail.com>
2023-09-13 08:13:53 -0300

    arrumando os filtros

--- src/pages/Cardapio/Filtros/index.tsx
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -4,11 +4,11 @@ import filtros from './filtros.json';
 type IOpcao = typeof filtros[0]
 
 
-export default function Filtros(opcao: IOpcao){
+export default function Filtros(){
   return (
-    <div className={styles.filtro}>
+    <div className={styles.filtros}>
       {filtros.map((opcao) => (
-        <button className={styles.filtro__filtro} key={opcao.id}>
+        <button className={styles.filtros__filtro} key={opcao.id}>
           {opcao.label}
         </button>
       ))}
